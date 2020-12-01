19c6946a849e07480bacc1cd2ced468f845838e0
Dylan Morgan <dbmorgan98@gmail.com>
2020-12-01 13:02:07 +0000

    Work on parse-1d

--- nmr_log_parser.py
+++ nmr_log_parser.py
@@ -7,35 +7,74 @@ Prints to new file
     """
 
     def __init__(self):
+        self.filename = None
         self.iso_values = []
+        self.xx_coors = []
+        self.xy_coors = []
+        self.xz_coors = []
 
-    def read_file(self):
+    def read_iso_value(self):
+        self.filename = input("Gaussian output file name (must be full path): ")
+        logline = []
 
-        filename = input("Gaussian output file name (must be full path): ")
-        log_line = []
-
-        with open(filename) as log_file:
-            for line in log_file:
+        with open(self.filename) as logfile:
+            for line in logfile:
                 if "Isotropic" in line and "Bq" in line:
-                    log_line.append(line.split())
+                    logline.append(line.split())
                         
-            for line in log_line:
+            for line in logline:
                 for word in line:
                     if word == line[4]:
                         self.iso_values.append(float(word))
 
-            #trying to append only the 4th item in line to iso_value
-
             print(self.iso_values)
-                       
-        #print all iso values 
-            
+
+    def read_xx_line(self):
+#        all_lines = []
+#        xxline = []
+        
+#        with open(self.filename) as logfile:
+#            for line in logfile:
+#                all_lines.append(line.split())
+
+#        for line in all_lines:
+#            if "Bq" and "Isotropic" in line:
+#                xxline.append(line)
+
+#        with open(self.filename) as original:
+#            with open("log_copy.txt", "w+") as copy:
+#                for line in original:
+#                    copy.write(line)
+                    
+#                for line in copy:
+#                    if "Bq" and "Isotropic" in line:
+#                        copy.write("")
+
     def copy_iso_values(self):
-        #copy isotropic values to new file
+        #copy isotropic values and coors to new file
         pass
 
 if __name__ == "__main__":
     lp = log_parser()
     print(lp.__doc__)
-    lp.read_file()
+    lp.read_iso_value()
+    lp.read_xx_line()
     
+
+
+"""
+def read_iso_value(self):
+    #possible alt to other func with same name
+
+    filename = input("Gaussian output file name (must be full path): ")
+    all_lines = []
+    logline = []
+    
+    with open(filename) as logfile:
+        for line in logfile:
+            logline.append(line.split())
+
+            if "Isotropic" in line and "Bq" in line:
+                logline.append(line)
+                print logline
+"""
