b1207a708080c14a89618bc225e32ad088b8c3a6
Dylan Morgan <dbmorgan98@gmail.com>
2020-12-08 14:18:31 +0000

    Add coors to parse-1d

--- nmr_log_parser.py
+++ nmr_log_parser.py
@@ -11,15 +11,25 @@ Glossary (in alphabetical order):
     '''
 
     def __init__(self):
-        self.filename = None
-        self.iso_values = []
+        self.inp_filename = None
+        self.out_filename = None
         self.coors = []
+        self.iso_values = []
+        self.tensors = []
+
+    def read_coors(self):
+        self.inp_filename = input('Gaussian input file name (must be full path): ')
+
+        with open(self.inp_filename) as comfile:
+            for line in comfile:
+                if 'Bq' in line:
+                    self.coors.append(line)
 
     def read_iso_values(self):
-        self.filename = input('Gaussian output file name (must be full path): ')
+        self.out_filename = input('Gaussian output file name (must be full path): ')
         logline = []
 
-        with open(self.filename) as logfile:
+        with open(self.out_filename) as logfile:
             for line in logfile:
                 if 'Isotropic' in line and 'Bq' in line:
                     logline.append(line.split())
@@ -29,45 +39,49 @@ Glossary (in alphabetical order):
                     if word == line[4]:
                         self.iso_values.append(float(word))
 
-    def read_coors(self):
-        with open(self.filename) as logfile:
+    def read_tensors(self):
+        with open(self.out_filename) as logfile:
             while True:
                 try:
                     line = next(logfile)
                     if 'Isotropic' in line and 'Bq' in line:
                         line = next(logfile)
-                        self.coors.append(line)
+                        self.tensors.append(line)
                         line = next(logfile)
-                        self.coors.append(line)
+                        self.tensors.append(line)
                         line = next(logfile)
-                        self.coors.append(line)
+                        self.tensors.append(line)
 
                 except StopIteration:
                     break
             
-        print(f'\nFinished parsing file {self.filename}\n')
+        print(f'\nFinished parsing file: {self.out_filename}\n')
 
     def copy_iso_values(self):
         '''
-        copy iso values and coors to new file
+        copy coors, iso values, and tensors to new file
         append Bq and count to each line
         '''
 
-        copy_filename = input('Name to save data as must be full path: ')
+        copy_out_filename = input('Name to save data as (must be full path): ')
 
-        with open(copy_filename, 'w+') as copy:
-            copy.write('\nNICS Isotropic Values:\n')
+        with open(copy_out_filename, 'w+') as copy:
+            copy.write('\nGhost Atom Coordinates:\n')
+            for count, line in enumerate(self.coors, 1):
+                copy.write(str(f'{count} {line}'))
+
+            copy.write('\n\nNICS Isotropic Values:\n')
             for count, line in enumerate(self.iso_values, 1):
                 copy.write(str(f'{count}  Bq:  {line}\n'))
 
-            copy.write('\nGhost Atom Coordinates:\n')
-            for line in self.coors:   
+            copy.write('\nGhost Atom Tensors:\n')
+            for line in self.tensors:   
                 if 'XZ' in line:
                     copy.write(f'{line}\n')
                 else:
                     copy.write(line)
         
-        with open(copy_filename, 'r') as copy:
+        with open(copy_out_filename, 'r') as copy:
             contents = copy.read()
             print(contents)
 
@@ -75,8 +89,9 @@ Glossary (in alphabetical order):
 if __name__ == '__main__':
     lp = log_parser()
     print(lp.__doc__)
-    lp.read_iso_values()
     lp.read_coors()
+    lp.read_iso_values()
+    lp.read_tensors()
     lp.copy_iso_values()
 
 
@@ -84,13 +99,14 @@ if __name__ == '__main__':
 TODO:
 [X] Print iso values as column
 [X] Copy lists to new file
-[X] Make read_coors() work
-[X] Make read_coors work() for other coors (and not just X) 
-[ ] Fix spacings between coors so there are no spaces between XX, XY, XZ and 1 between each Bq
+[X] Make read_tensors() work
+[X] Make read_tensors work() for other tensors (and not just X) 
+[(x)] Fix spacings between tensors so there are no spaces between XX, XY, XZ and 1 between each Bq
     # Maybe can use: 
     # for n in range:
     # from inp_gen_1d
-[ ] Print each Bq atom with its label (eg. 4 Bq [then coors here])
+[N/A] Print each Bq atom with its label (eg. 4 Bq [then tensors here])
+[ ] Fix example files. Benzene is given as input and anthracene is given as output
 
 
 NOTE:
@@ -99,6 +115,7 @@ pop:
 /home/dylanmorgan/python/test/parsed_nmr_anthracene.txt
 
 manjaro:
+/home/pop!_os/home/dylanmorgan/python/test/benzene_opt_target.com
 /home/pop!_os/home/dylanmorgan/python/test/nmr_anthracene.log
 /home/pop!_os/home/dylanmorgan/python/test/parsed_nmr_anthracene.txt
 '''
