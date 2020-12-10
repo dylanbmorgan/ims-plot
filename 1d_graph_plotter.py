334e4e2143fbabb006ebd7a0c7ede4b9abaa8358
Felix Plasser (lin) <f.plasser@lboro.ac.uk>
2020-12-10 11:49:43 +0000

    fix for plotting script

--- 1d_graph_plotter.py
+++ 1d_graph_plotter.py
@@ -28,34 +28,11 @@ class plotter:
 
         with open('parsed_log_data') as data:
             for line in data:
+                words = line.split()
                 if 'cBq' in line:
-                    xcoor_line.append(line.split())
+                    self.xvalues.append(float(words[axis]))
                 elif 'iBq' in line:
-                    ycoor_line.append(line.split())
-
-            for line in ycoor_line:
-                for word in line:
-                    if word == line[2]:
-                        self.yvalues.append(float(word))
-            
-            for line in xcoor_line:
-                for word in line:
-                    if word == line[axis] and word != '0.0':
-                        self.xvalues.append(float(word))
-                    elif word == '0.0' and word != '10.0' and word == line[axis]:
-                        zero = len(xcoor_line)/2
-
-                        if zero % 2 == '0':
-                            self.xvalues.insert(int(zero), '0.0')
-                        elif zero % 2 != '0':
-                            zero_odd = zero + 0.5 
-                            self.xvalues.insert(int(zero_odd), '0.0')
-
-                        # If 0.0 is not excluded, will add >1 0.0 value to xvalues list.
-                        # Only occurs if there is a 0.0 value in ghost atom coordinates.
-                        # As a workaround, exclude 0.0 from being added to the list, 
-                        # and divide length of list by 2.
-                        # Add 0.0 to this point in the list 
+                    self.yvalues.append(float(words[2]))
 
         print('x-axis:', *self.xvalues, sep = ' ')  #remove when committing 
         print('y-axis:', *self.yvalues, sep = ' ')  #remove when committing 
