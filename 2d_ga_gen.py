d0410a71752fa2569b766bbd57e0e2386d1eb962
Dylan Morgan <dbmorgan98@gmail.com>
2020-12-15 17:37:12 +0000

    Start 2d

---
+++ 2d_ga_gen.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import sys
+import click
+import numpy 
+
+
+# TODO:
+# [ ] create a CLI useable script where all of these scripts can be called using arvis
+# [ ] options for each script (eg. arvis -g for ga generator)
+# [ ] syntax: arvis -g  <first_ga_coor> <coor_step> <no_of_coors> <filename_to_copy> <copied_filename>
+# [ ] (optional) autogenerate copied file name
+
+'''
+class input_generator
+
+    def __init__(self):
+        pass
+
+    def bq_coors():
+        input 
+    '''
+
+
+if __name__ == "__main__":
+    
