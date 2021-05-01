ae2a50d6ff7b79da4d55afab786aad4a50e27947
Dylan Morgan <dbmorgan98@gmail.com>
2021-05-01 21:10:58 +0100

    Updated ga_gen and started 3d_graph #16

---
+++ scripts/3d_graph.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# 2d_graph_plotter.py
+# Plots contour plot of isotropic NICS values from parsed file data
+# Author: Dylan Morgan
+
+import matplotlib.pyplot as plt
+import argparse
+import glob
+import sys
+
+class Plotter:
+
